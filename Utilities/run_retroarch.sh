99daa631ebfa35561ab7992826d9e68a39f25deb
Wintermute0110 <wintermute0110@gmail.com>
2017-09-09 02:44:23 +0900

    Fix log redirections.

--- Utilities/run_retroarch.sh
+++ Utilities/run_retroarch.sh
@@ -22,7 +22,7 @@ kodi_rpc_params()
 }
 
 # --- Print some information ---
-echo "Retroarch core  \"$1\"" >> $LOG_FILE
+echo "Retroarch core  \"$1\"" > $LOG_FILE
 echo "ROM filename    \"$2\"" >> $LOG_FILE
 echo "Category ID     \"$3\"" >> $LOG_FILE
 echo "Launcher ID     \"$4\"" >> $LOG_FILE
@@ -37,7 +37,7 @@ if [ ! -f "$core_file" ]; then
     kodi_rpc_params "GUI.ShowNotification" "$PARAMS"
     exit 1
 fi
-echo "Core \"$core_file\" found."  >> $LOG_FILE
+echo "Core \"$core_file\" found." >> $LOG_FILE
 
 # --- Check if retroarch executable exists ---
 if [ ! -f "$RETROARCH" ]; then
@@ -45,33 +45,33 @@ if [ ! -f "$RETROARCH" ]; then
     kodi_rpc_params "GUI.ShowNotification" "$PARAMS"
     exit 1
 fi
-echo "Retroarch executable \"$RETROARCH\" found."  >> $LOG_FILE
+echo "Retroarch executable \"$RETROARCH\" found." >> $LOG_FILE
 
 # --- In LibreELEC use this ---
-# echo "Shutting down Kodi (LibreELEC) ..."  >> $LOG_FILE
+# echo "Shutting down Kodi (LibreELEC) ..." >> $LOG_FILE
 # systemctl stop kodi
 
 # --- In Kodibuntu kill the Kodi process after shutdown ---
-echo "Shutting down Kodi (Linux) ..."  >> $LOG_FILE
+echo "Shutting down Kodi (Linux) ..." >> $LOG_FILE
 kodi_rpc "Application.Quit"
 sleep 2
 killall -9 kodi
 killall -9 kodi.bin
 
-echo "Running Retroarch ..."  >> $LOG_FILE
-$RETROARCH -L $core_file -v "$2" >> $LOG_RETROARCH
+echo "Running Retroarch ..." >> $LOG_FILE
+$RETROARCH -L $core_file -v "$2" > $LOG_RETROARCH 2>&1
 
 # --- In LibreELEC use this ---
-# echo "Starting Kodi (LibreELEC) ..."  >> $LOG_FILE
+# echo "Starting Kodi (LibreELEC) ..." >> $LOG_FILE
 # systemctl start kodi
 
 # --- In Kodibuntu use this ---
-echo "Starting Kodi (Linux) ..."  >> $LOG_FILE
+echo "Starting Kodi (Linux) ..." >> $LOG_FILE
 /usr/bin/kodi &
 sleep 2
 
 # --- Tell Kodi to open AEL in the correct Launcher ---
-echo "Starting AEL Launcher ..."  >> $LOG_FILE
+echo "Starting AEL Launcher ..." >> $LOG_FILE
 ael_id="\"addonid\":\"plugin.program.advanced.emulator.launcher\""
 ael_pars="\"params\":{\"com\":\"SHOW_ROMS\",\"catID\":\"$3\",\"launID\":\"$4\"}"
 PARAMS="\"params\":{$ael_id,$ael_pars}"
